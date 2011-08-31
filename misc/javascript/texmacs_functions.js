68fb8de3e9604128a2e190742a4862ff617a028a
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2011-08-31 14:25:00 +0000

    Add Javascript functions for TeXmacs

---
+++ misc/javascript/texmacs_functions.js
@@ -0,0 +1,31 @@
+
+function detectOS () {
+  if (navigator.userAgent.indexOf ('Linux') != -1) return "Linux";
+  else if (navigator.userAgent.indexOf ('Mac') != -1) return "MacOS";
+  else if (navigator.userAgent.indexOf ('Win') != -1) return "Windows";
+  else return "Unknown";
+}
+
+function downloadPage () {
+  var OS = detectOS ();
+  if (OS == "Linux")
+    return "http://www.texmacs.org/tmweb/download/unix.en.html";
+  else if (OS == "MacOS")
+    return "http://www.texmacs.org/tmweb/download/macosx.en.html";
+  else if (OS == "Windows")
+    return "http://www.texmacs.org/tmweb/download/windows.en.html";
+  else
+    return "http://www.texmacs.org/tmweb/download/download.en.html";
+}
+
+function downloadLink (text) {
+  return "<A href=\"" + downloadPage () + "\">" + text + "</A>";
+}
+
+function downloadButton () {
+  return downloadLink ("Download");
+}
+
+function downloadButtonImage () {
+  return downloadLink ("<img border=0 src=\"http://www.texmacs.org/Images/collage.png\"></img>");
+}
